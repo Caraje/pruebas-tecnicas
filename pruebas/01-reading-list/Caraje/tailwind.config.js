50ce950db1d1152b5b47b26c33928b808d91f0ee
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-07-15 21:29:50 +0200

    add font family Montserrat and, color custom

--- pruebas/01-reading-list/Caraje/tailwind.config.js
+++ pruebas/01-reading-list/Caraje/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat']
+      }
+    },
+    colors: {
+      'caraje-white': '#fefefe',
+      'caraje-grey': '#DFE2E8',
+      'caraje-darkBeige': '#46443C',
+      'caraje-Beige': '#96988C',
+      'caraje-lightBeige': '#D2C9A1',
+      'caraje-salmon': ' #DF8767'
+
+    }
   },
   plugins: []
 }
